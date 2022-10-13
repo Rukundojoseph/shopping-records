cfd3aebcba01bbdffc165eb808db259bf351df7f
Rukundojoseph <josephrukundo2002@gmail.com>
2022-10-13 09:12:39 +0200

    allowed trim words to be the same

--- app.js
+++ app.js
@@ -13,21 +13,21 @@ var number=1;
 submitt.addEventListener('click',()=>{
     let total=0     
     total=parseInt(unitper.value)*parseInt(price.value); 
-    html=`<tr id=${description.value}><td>${number}</td><td>${description.value}</td><td>${price.value}</td><td>${unitper.value}</td><td>${total}</td></tr>`   ;
-    if (dp.indexOf(description.value)==-1){    
+    html=`<tr id=${description.value.trim()}><td>${number}</td><td>${description.value}</td><td>${price.value}</td><td>${unitper.value}</td><td>${total}</td></tr>`   ;
+    if (dp.indexOf(description.value.trim())==-1){    
         number+=1
     }   
    
-    if (dp.indexOf(description.value)==-1){
+    if (dp.indexOf(description.value.trim())==-1){
     table1.innerHTML+=html;
-    dp.push(description.value)
+    dp.push(description.value.trim())
     alltotal.push(total)
     }
     else{
         let newtotal=parseInt(unitper.value)*parseInt(price.value);
         number=dp.indexOf(description.value)+1      
         alltotal[dp.indexOf(description.value)]=newtotal ;      
-        document.getElementById(description.value).innerHTML=`<td>${number}</td><td>${description.value}</td><td>${price.value}</td><td>${unitper.value}</td><td>${total}</td>`
+        document.getElementById(description.value.trim()).innerHTML=`<td>${number}</td><td>${description.value}</td><td>${price.value}</td><td>${unitper.value}</td><td>${total}</td>`
        console.log(newtotal)
     }
 
